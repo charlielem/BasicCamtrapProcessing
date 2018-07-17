319d692ca86f7083b219cd0b889923346f682636
charlielem <41323081+charlielem@users.noreply.github.com>
2018-07-17 12:13:04 +0100

    Add files via upload

---
+++ RP_2017_Attempt1.R
@@ -0,0 +1,34 @@
+#Tuesday 17th July 2018
+#Regents Park 2017 Data
+
+#code to clear workspace if needed:
+rm(list = ls())
+
+library(data.table)
+
+setwd("C:/Users/cml1917/Desktop/IoZ/Analysis")
+
+#source("Data_tools.R") # this step is erroring, not sure what I need to change to make it work - does this just mean 
+#what script was used/where the info came from???
+
+infolder <- "C:/Users/cml1917/Desktop/IoZ/Analysis/Regents_Park_2017"
+outfile <- "C:/Users/cml1917/Desktop/IoZ/Analysis/metadata.csv"
+exifolder <- ("C:/Users/cml1917/Desktop/IoZ/Analysis/exiftool.exe")
+
+#read.exif(infolder, outfile, exifolder) = did this line first as was in script I was following but said could not find fucntion read.exif
+
+read.exif <- function(infolder, outfile, exifpath="C:/Users/Rowcliffe.M/Documents/APPS/ExifTool"){
+  wd <- getwd()
+  setwd(exifpath)
+  cmd <- paste("exiftool -r -csv", infolder, ">", outfile)
+  shell(cmd)
+  read.csv(outfile)
+  setwd(wd)
+}
+
+
+imgdat <- read.csv(outfile)
+
+
+
+
